d29a98fe09eb612fd4dcdd1f13e95bdca7db6370
Alperen Topcu <alperenttpc78@gmail.com>
2025-06-07 11:30:36 +0300

    model + repository

---
+++ src/main/java/com/example/jwt_demo/model/User.java
@@ -0,0 +1,20 @@
+package com.example.jwt_demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+}
